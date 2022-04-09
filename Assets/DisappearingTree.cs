2bb511f0debf60aa17fe007341df51c4e672d926
aChallengr <justin.a.gast@gmail.com>
2022-04-09 10:07:39 -0400

    Level1 Implement (in progress)
    not done yet

---
+++ Assets/DisappearingTree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingTree : MonoBehaviour
+{
+    bool disappearing;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!disappearing)
+        {
+            disappearing = true;
+            StartCoroutine(StartDestruction());
+        }
+    }
+
+    IEnumerator StartDestruction()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(true);
+    }
+}
