a10c096a87bb6b4268f7dd60ac5fe03cd6a26fd1
aChallengr <justin.a.gast@gmail.com>
2022-04-17 17:23:16 -0400

    Level2 in progress
    need to figure out camera situation

--- Assets/Scripts/Environment/Keylock.cs
+++ Assets/Scripts/Environment/Keylock.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Keylock : MonoBehaviour
 {
     public string keyName;
     public Vector3 KeyOffset;
-    private bool active = false;
-    
+
+    public UnityEvent activate;
+    public UnityEvent deactivate;
+    public GameObject key;
+
     private void OnTriggerEnter(Collider collision)
     {
-        var obj = collision.gameObject;
-        if (obj.CompareTag("Item") && obj.name.Equals(keyName) && !obj.GetComponent<Key>().CheckHeld())
+        if (collision.CompareTag("Item") && !collision.GetComponent<Key>().CheckHeld())
         {
             // Position key
-            obj.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            obj.GetComponent<Rigidbody>().useGravity = false;
-            obj.transform.SetParent(gameObject.transform);
-            obj.transform.position = gameObject.transform.position + KeyOffset;
+            key = collision.gameObject;
+            key.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            key.GetComponent<Rigidbody>().useGravity = false;
+            key.transform.SetParent(gameObject.transform);
+            key.transform.position = gameObject.transform.position + KeyOffset;
 
             //Activate key
-            obj.GetComponent<Key>().Activate();
+            Activate();
         }
+    }
+
+    private void Update()
+    {
+        if (key != null && key.GetComponent<Key>().CheckHeld())
+        {
+            Deactivate();
+            key = null;
+        }
+    }
+
+    public void Activate()
+    {
+        activate.Invoke();
+    }
+
+    // Deactivate should return to status-quo
+    public void Deactivate()
+    {
+        deactivate.Invoke();
     }
 }
